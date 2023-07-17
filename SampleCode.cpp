1db14070136bf118543932765655dab0729990b1
Abel <belaashu23@gmail.com>
2023-07-17 16:03:11 +0200

    Added a Multiplier

--- SampleCode.cpp
+++ SampleCode.cpp
@@ -8,6 +8,10 @@ int Subtractor (int a, int b){
     return a - b; 
 } 
 
+int Multiplier (int a, int b){
+    return a * b; 
+}
+
 int main () {
     std::cout << "Yoww what a beautiful Monday" << std::endl; 
     std::cout << "This is the second line of code" << std::endl; 
