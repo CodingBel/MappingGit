f7fbf86bfd11b9bda0a9a56fd0c3f347112abd17
Abel <belaashu23@gmail.com>
2023-07-17 14:51:35 +0200

    Added an Adder Function

--- SampleCode.cpp
+++ SampleCode.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+int adder (int a, int b){
+    return a + b; 
+}
+
 int main () {
     std::cout << "Yoww what a beautiful Monday" << std::endl; 
     std::cout << "This is the second line of code" << std::endl; 
