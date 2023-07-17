c89ff90426b6bda33f780bec11e91b6abfa3634b
Abel <belaashu23@gmail.com>
2023-07-17 14:11:24 +0200

    Added a third line of code

--- SampleCode.cpp
+++ SampleCode.cpp
@@ -3,6 +3,7 @@
 int main () {
     std::cout << "Yoww what a beautiful Monday" << std::endl; 
     std::cout << "This is the second line of code" << std::endl; 
+    std::cout << "This is the third line of code" << std::endl; 
 
     return 0; 
 }
